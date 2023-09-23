c7941ad3091e39a362e6f1bfe5e9905dfea44428
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2023-09-24 00:22:00 +0100

    Collision detection introduced, see desc. for more
    - defaultGroup is now draggable
    - some other minor changes mainly towards code readability

--- main.js
+++ main.js
@@ -29,36 +29,35 @@ const
     }
   })
   ,
-  groupStroke = new Rect({
+  defaultGroupStroke = new Rect({
     strokeWidth: 10,
-    stroke: 'purple',
+    stroke: 'grey',
   })
 
-globalThis.defaultStage = new Stage( StageDefaults.getAttrs() ).add(
+globalThis.defaultStage = new Stage( { ...StageDefaults.getAttrs() } ).add(
 
-  globalThis.defaultLayer = new Layer( {name: Object.keys({Default} )[0] + Layer.name}).add(
+  globalThis.defaultLayer = new Layer( { name: Object.keys({Default} )[0] + Layer.name, draggable: true }).add(
 
-    groupStroke,
+    defaultGroupStroke,
     globalThis.defaultGroup = new Group( 
       {
         name: Object.keys({Default} )[0] + Group.name,
         x: StageDefaults.width() / 4,
         y: StageDefaults.height() / 4,
-        draggable: true,
       }
     ).add(
 
       // GOAL: check if B's resultant coords (i.e. resultant ordered pair for each component x,y are equal or less width, height of A - if so then it collides)
 
       new Shape({
-        name: 'shape-of-target', /* (shapeA) */
+        name: 'shapeA',
         ...BoundingBoxDefaults.getAttrs()
         ,
         fill: 'lightgreen'
       })
       ,
       new Shape({
-        name: 'shape-of-source', /* (shapeB)  */
+        name: 'shapeB',
         ...BoundingBoxDefaults.getAttrs()
         ,
         fill: 'orange',
@@ -75,8 +74,8 @@ if(defaultGroup.hasChildren){
 
   let incrementalHeight = 0;
   let defaultVerticalPadding = BoundingBoxDefaults.y();
-  groupStroke.setAttrs( defaultGroup.getClientRect() )
-  groupStroke.setAttr('height', (BoundingBoxDefaults.height()*defaultGroup.getChildren().length) )
+  defaultGroupStroke.setAttrs( defaultGroup.getClientRect() )
+  defaultGroupStroke.setAttr('height', (BoundingBoxDefaults.height()*defaultGroup.getChildren().length) )
   defaultGroup.getChildren().forEach((each)=>{
     each.setAttrs({
       y: incrementalHeight+defaultVerticalPadding,
@@ -88,11 +87,13 @@ if(defaultGroup.hasChildren){
 
   const [shapeA, shapeB] = defaultGroup.getChildren();
   const shapeA$initFill = shapeA.getAttr('fill')
+  const shapeB$initDims = shapeB.getClientRect({relativeTo: defaultGroup})
 
   shapeB.on('dragmove', function(){
     // DEV_NOTE # Without Math.abs collision would work only for right and bottom sides
     if ( Math.abs(this.x()) <= shapeA.width() && Math.abs(this.y()) <= shapeA.height() ){
-      shapeA.setAttr('fill', 'red')
+      /* shapeA.setAttr('fill', 'red') */// instead do something more restrictive such as forced position resetting for shapeB (orange) once collided with shapeA (green)
+      shapeB.setAttrs({ x: shapeB$initDims.x, y: shapeB$initDims.y  })
     }
     else {
       shapeA.setAttr('fill', shapeA$initFill)
